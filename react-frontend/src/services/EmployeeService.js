dc215939dba2a517323aff0b02ca5eac787f899c
Jumanahalhazba <97724418+Jumanahalhazba@users.noreply.github.com>
2022-02-28 06:13:23 +0200

    Front end
    Routing

---
+++ react-frontend/src/services/EmployeeService.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const EMPLOYEE_API_BASE_URL = "http://localhost:8084/api/v1/employees";
+
+// import axios from "axios";
+// axios.create({
+//   baseURL: "http://localhost:8084/api/v1/employees",
+//   headers: {
+//     "Content-type": "application/json"
+//   }
+// });
+
+class EmployeeService {
+
+    getEmployees(){
+        return axios.get(EMPLOYEE_API_BASE_URL);
+    }
+
+    createEmployee(employee){
+        return axios.post(EMPLOYEE_API_BASE_URL, employee);
+    }
+
+    getEmployeeById(employeeId){
+        return axios.get(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    }
+
+    updateEmployee(employee, employeeId){
+        return axios.put(EMPLOYEE_API_BASE_URL + '/' + employeeId, employee);
+    }
+
+    deleteEmployee(employeeId){
+        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    }
+}
+
+export default new EmployeeService()
