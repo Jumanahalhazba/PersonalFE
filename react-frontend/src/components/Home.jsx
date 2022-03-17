1c28a84d3e021606cdaa82dfcb26fc3ad45b8f93
Jumanahalhazba <jal-hazba@retrorabbit.co.za>
2022-03-17 14:49:46 +0100

    Display image and modal test

--- react-frontend/src/components/Home.jsx
+++ react-frontend/src/components/Home.jsx
@@ -1,19 +1,50 @@
 import React from "react";
 import test1 from "../../src/components/test1.jpeg";
 import test2 from "../../src/components/test2.jpg";
+import {Modal,Button} from 'react-bootstrap'; 
+
 const Home = () => {
   
   return (
     <div>
     <h2 className="text-center">SELECT MEMBER</h2>
-    {/* <img src="test1.png" class="rounded float-left" alt="test1"></img> */}
-    <div className="image-click">
     
+    <div className="image-click">
     <a href='/employees'><img src={test1} class="rounded float-left" alt="pic1" style={{ paddingLeft: 10 ,maxWidth: 150}}></img></a>
     </div>
-    <img src={test2} class="rounded float-left" alt="pic2" style={{paddingLeft: 10 ,maxWidth: 150}}></img>
     
+    <img src={test2} class="rounded float-left" alt="pic2" style={{paddingLeft: 10 ,maxWidth: 150}}></img>
+
+    {/* <div> 
+
+    <Button className="modal-test">Open Modal</Button>  
+
+    <Modal show={false}> </Modal>
+
+    <Modal.Header>Modal Head Part</Modal.Header>
+
+    <Modal.Body> React Modal Working </Modal.Body>
+
+    <Modal.Footer>
+      <Button> Close </Button>
+    </Modal.Footer>
+
+    </div> */}
+
+    {/* <div>
+      <a href = {link}>
+        <img src= {image} alt= 'icon' />
+      </a>
+    </div>  */}
+
+    <body>
+        <label>View Photo</label>
+        Title: <span text="${title}">name</span>
+        <img alt="sample" src="*{'data:image/png;base64,'+image}" />
+    </body>
+
     </div>
+    
   );
 };
 
